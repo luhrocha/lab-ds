4bd1e394975e4dc4421a038a431dcd0ab777434f
luhrocha <luanarocha17@gmail.com>
2022-10-16 18:31:31 -0300

    add test, api/msw

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { useState } from 'react';
+import {SignIn} from './pages/SignIn';
 import './styles/global.css';
 
 export function App() {
   return (
-    <h1 className="font-bold text-red-400 p-5">olá de novo</h1>
-  )
+      <SignIn/>
+  );
 };
-
--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -4,12 +4,12 @@ import {Check} from 'phosphor-react';
 
 import { ReactNode } from 'react';
 
-export interface CheckboxProps{
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps{
 }
 
 export function Checkbox(props: CheckboxProps){
     return (
-        <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded">
+        <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded" {...props}>
             <CheckboxPrimitive.CheckboxIndicator asChild>
                 <Check weight='bold' className='h-5 w-5 text-cyan-500' />
             </CheckboxPrimitive.CheckboxIndicator>
